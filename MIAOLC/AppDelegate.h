506768b345bb86371201c3ba68f498d82a0eb346
qianfeng <qianfeng@Sandy.local>
2015-10-09 16:33:35 +0800

    Initial Commit

---
+++ MIAOLC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MIAOLC
+//
+//  Created by qianfeng on 15/10/9.
+//  Copyright (c) 2015年 苗军艳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
